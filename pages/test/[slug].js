663b47be17a97f77c167b1644cede85816082b9f
Henry Smith <henry.smith@netlify.com>
2023-04-11 13:30:34 +0100

    First Commit

---
+++ pages/test/[slug].js
@@ -0,0 +1,28 @@
+export const getStaticProps = ({ params }) => {
+  const { slug } = params;
+  return {
+    props: {
+      id: slug,
+    },
+    revalidate: 60,
+  };
+};
+
+export const getStaticPaths = () => {
+  let paths = [];
+  for (var i = 1; i <= 10000; i++) {
+    paths.push(`/test/${i}`);
+  }
+  return { paths, fallback: "blocking" };
+};
+
+const Page = ({ id }) => {
+  return (
+    <div>
+      <h1>This is page number {`${id}`}</h1>
+      <img src={`https://source.unsplash.com/random/800x800/?img=${id}`} />
+    </div>
+  );
+};
+
+export default Page;
