eb87c660964a992ea0808b9312ae56f7a4c0372a
Henry Smith <henry.smith@netlify.com>
2023-04-11 13:56:09 +0100

    Turn of SWC minify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
     return "test";
